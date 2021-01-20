a6d4353b207e74cd0f434e3c4dc5b20c8946e51a
colin <colinzzz43@gmail.com>
2021-01-19 19:31:29 -0800

    using the template game engine instead, so everyone can understand better

--- Game/menu.js
+++
@@ -1,8 +0,0 @@
-export default class menu {
-  // This class will eventually be merged into the game class.
-  constructor() {
-    // Start/ Pause button
-    // Stop button
-    // Restart button
-  }
-}
