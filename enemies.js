39de7b114d346d0a0af55c0e91ee3d320cfe95a7
Daniel Ty <tydaniel270@gmail.com>
2021-01-19 22:03:06 -0800

    added sprites folder, created skeleton for structures, enemies, and hud

---
+++ enemies.js
@@ -0,0 +1,157 @@
+class Slime {
+    constructor() {
+
+        this.spritesheet = ASSETMANAGER.getAsset("./sprites/monster/slime1_front.png");
+
+    }
+
+    updateBB() {
+
+    }
+
+    update() {
+
+    }
+
+    draw() {
+
+    }
+
+    attack() {
+
+    }
+
+    isDead() {
+
+    }
+}
+
+class Goblin {
+    constructor() {
+
+    }
+
+    updateBB() {
+        
+    }
+
+    update() {
+
+    }
+
+    draw() {
+
+    }
+
+    attack() {
+        
+    }
+
+    isDead() {
+
+    }
+}
+
+class Skeleton {
+    constructor() {
+
+    }
+
+    updateBB() {
+        
+    }
+
+    update() {
+
+    }
+
+    draw() {
+
+    }
+
+    attack() {
+        
+    }
+
+    isDead() {
+
+    }
+}
+
+class Eyeball {
+    constructor() {
+
+    }
+
+    updateBB() {
+        
+    }
+
+    update() {
+
+    }
+
+    draw() {
+
+    }
+
+    attack() {
+        
+    }
+
+    isDead() {
+
+    }
+}
+
+class Mushroom {
+    constructor() {
+
+    }
+
+    updateBB() {
+        
+    }
+
+    update() {
+
+    }
+
+    draw() {
+
+    }
+
+    attack() {
+        
+    }
+
+    isDead() {
+
+    }
+}
+
+class Dragon {
+    constructor() {
+
+    }
+
+    updateBB() {
+        
+    }
+
+    update() {
+
+    }
+
+    draw() {
+
+    }
+
+    attack() {
+        
+    }
+
+    isDead() {
+
+    }
+}
---
+++ hud.js
@@ -0,0 +1,6 @@
+class Hud {
+    constructor() {
+
+    }
+
+}
