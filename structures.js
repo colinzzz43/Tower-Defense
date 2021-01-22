ed97d5f0a763f90dcc28bd87b54bf742446c9b8c
Brandon Kennedy <52840951+brandk94@users.noreply.github.com>
2021-01-22 13:40:31 -0800

    Updated branch to make it look like master branch

---
+++ structures.js
@@ -0,0 +1,332 @@
+class Base {
+    constructor() {
+
+        this.spritesheet = ASSET_MANAGER.getAsset("./sprites/other/base.png");
+
+    }
+
+    draw() {
+
+    }
+
+    isDead() {
+
+    }
+}
+
+class PistolTower {
+    constructor() {
+
+        this.spritesheet = ASSET_MANAGER.getAsset("./sprites/towers/pistol/tower_pistol_down.png");
+
+    }
+
+    draw() {
+
+    }
+
+    buy() {
+
+    }
+
+    sell() {
+
+    }
+
+    upgrade() {
+
+    }
+
+    aim() {
+
+    }
+
+    fireProjectile() {
+
+    }
+
+    isDead() {
+
+    }
+
+}
+
+class MachineGunTower {
+    constructor() {
+
+    }
+
+    draw() {
+
+    }
+  
+    buy() {
+
+    }
+
+    sell() {
+        
+    }
+
+    upgrade() {
+
+    }
+
+    aim() {
+
+    }
+
+    fireProjectile() {
+
+    }
+
+    isDead() {
+
+    }
+
+}
+
+class ShotgunTower {
+    constructor() {
+
+    }
+
+    draw() {
+
+    }
+  
+    buy() {
+
+    }
+
+    sell() {
+        
+    }
+
+    upgrade() {
+
+    }
+
+    aim() {
+
+    }
+
+    fireProjectile() {
+
+    }
+
+    isDead() {
+
+    }
+
+}
+
+class CannonTower {
+    constructor() {
+
+    }
+
+    draw() {
+
+    }
+  
+    buy() {
+
+    }
+
+    sell() {
+        
+    }
+
+    upgrade() {
+
+    }
+
+    aim() {
+
+    }
+
+    fireProjectile() {
+
+    }
+
+    isDead() {
+
+    }
+
+}
+
+class FlamethrowerTower {
+    constructor() {
+
+    }
+
+    draw() {
+
+    }
+  
+    buy() {
+
+    }
+
+    sell() {
+        
+    }
+
+    upgrade() {
+
+    }
+
+    aim() {
+
+    }
+
+    fireProjectile() {
+
+    }
+
+    isDead() {
+
+    }
+
+}
+
+class LaserTower {
+    constructor() {
+
+    }
+
+    draw() {
+
+    }
+  
+    buy() {
+
+    }
+
+    sell() {
+        
+    }
+
+    upgrade() {
+
+    }
+
+    aim() {
+
+    }
+
+    fireProjectile() {
+
+    }
+
+    isDead() {
+
+    }
+
+}
+
+class MatterTower {
+    constructor() {
+
+    }
+
+    draw() {
+
+    }
+  
+    buy() {
+
+    }
+
+    sell() {
+        
+    }
+
+    upgrade() {
+
+    }
+
+    aim() {
+
+    }
+
+    fireProjectile() {
+
+    }
+
+    isDead() {
+
+    }
+
+}
+
+class RocketTower {
+    constructor() {
+
+    }
+
+    draw() {
+
+    }
+  
+    buy() {
+
+    }
+
+    sell() {
+        
+    }
+
+    upgrade() {
+
+    }
+
+    aim() {
+
+    }
+
+    fireProjectile() {
+
+    }
+
+    isDead() {
+
+    }
+
+}
+
+class SpazerTower {
+    constructor() {
+
+    }
+
+    draw() {
+
+    }
+  
+    buy() {
+
+    }
+
+    sell() {
+        
+    }
+
+    upgrade() {
+
+    }
+
+    aim() {
+
+    }
+
+    fireProjectile() {
+
+    }
+
+    isDead() {
+
+    }
+
+}
