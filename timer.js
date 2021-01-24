f4e0be81e52d86386aba6d62599287507cb0b426
Brandon Kennedy <52840951+brandk94@users.noreply.github.com>
2021-01-23 22:31:26 -0800

    Update timer.js

--- timer.js
+++ timer.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // This game shell was happily modified from Googler Seth Ladd's "Bad Aliens" game and his Google IO talk in 2011
 
 class Timer {
@@ -18,24 +17,3 @@ class Timer {
         return gameDelta;
     };
 };
-=======
-// This game shell was happily modified from Googler Seth Ladd's "Bad Aliens" game and his Google IO talk in 2011
-
-class Timer {
-    constructor() {
-        this.gameTime = 0;
-        this.maxStep = 0.05;
-        this.lastTimestamp = 0;
-    };
-
-    tick() {
-        var current = Date.now();
-        var delta = (current - this.lastTimestamp) / 1000;
-        this.lastTimestamp = current;
-
-        var gameDelta = Math.min(delta, this.maxStep);
-        this.gameTime += gameDelta;
-        return gameDelta;
-    };
-};
->>>>>>> master
