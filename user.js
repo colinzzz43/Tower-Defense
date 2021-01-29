470e9fdbab69d9d0c01200c6590a0217fecc0101
Brandon Kennedy <52840951+brandk94@users.noreply.github.com>
2021-01-29 00:37:42 -0800

    More updates to make this branch like master

--- user.js
+++ user.js
@@ -1,7 +1,30 @@
 class User {
-  constructor() {
+  constructor(gameEngine) {
+    Object.assign(this, { gameEngine });
+    this.gameEngine.user = this;
+    this.removeFromWorld = false;
+
     // initialization for the user
     this.balance = 100;
     this.hp = 100;
   }
+
+  // it is to be used for selling/ after killing enemies
+  increaseBalance(amount) {
+    this.balance += amount;
+    this.printCoins(this.balance);
+  }
+
+  // it is to be used for buying
+  decreaseBalance(amount) {
+    this.balance -= amount;
+    console.log("After decreased:" + this.balance);
+  }
+
+  printCoins(balance) {
+    document.getElementById("printCoins").innerHTML = balance;
+  }
+
+  draw() {}
+  update() {}
 }
